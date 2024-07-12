8763e947dfc5dfcae376a912fb8c32777952a1f8
Alam418 <fachrysyahalam@gmail.com>
2024-07-12 23:16:28 +0700

    Update

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,44 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { Col, Row, Container } from 'react-bootstrap'
-import headerImg from "../assets/media/background.png"
+import React from 'react'
+import { Container } from 'react-bootstrap'
 
 export const Banner = () => {
-    // const [loopNum, setLoopNumber] = useState(0);
-    // const [isDeleting, setIsDeleting] = useState(false);
-    // const toRotate = ["kami", "bukan", "wibu"];
-    // const [text, setText] = useState('');
-    // const [delta, setDelta] = useState(300 - Math.random() * 100);
-    // const period = 2000;
-
-    // useEffect(() => {
-    //     const ticker = setInterval(() => {
-    //         tick();
-    //     }, delta);
-
-    //     return () => { clearInterval(ticker) };
-    // }, [text])
-
-    // const tick = () => {
-    //     let i = loopNum % toRotate.length;
-    //     let fullText = toRotate[i];
-    //     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    //     setText(updatedText);
-    //     if (isDeleting) {
-    //         setDelta(prevDelta => prevDelta / 2)
-    //     }
-
-    //     if (!isDeleting && updatedText === fullText) {
-    //         setIsDeleting(true);
-    //         setDelta(period);
-    //     } else if (isDeleting && updatedText === '') {
-    //         setIsDeleting(false);
-    //         setLoopNumber(loopNum + 1);
-    //         setDelta(500);
-    //     }
-
-    // }
-
 
     return (
         <section className='banner' id='home'>
